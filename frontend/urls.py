5cf42f2ecc33dec0f58601da25c6168ee20c7115
Naitik Parmar <naitik@Naitiks-MacBook-Pro-2.local>
2022-11-19 01:41:41 +0530

    Added pages from old repo, static files configured

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view()),
-    path('events/', views.EventsPageView.as_view()),
-    path('guests_and_prices/', views.GuestsAndPricesPageView.as_view()),
-    path('team_and_contact/', views.TeamAndContactPageView.as_view()),
-    path('scores_and_fixtures/', views.ScoresAndFixturesPageView.as_view()),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('events/', views.EventsPageView.as_view(), name='events'),
+    path('guests_and_prices/', views.GuestsAndPricesPageView.as_view(), name='guests_and_prices'),
+    path('team_and_contact/', views.TeamAndContactPageView.as_view(), name='team_and_contact'),
+    path('scores_and_fixtures/', views.ScoresAndFixturesPageView.as_view(), name='scores_and_fixtures'),
 ]
