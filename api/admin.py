d88419eded30b26aeb5645966c10b53f0798c3fb
Nishika Solanki <solanki@Nishikas-MacBook-Air.local>
2022-11-29 14:24:27 -0400

    Migration added for contact us

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Sport
+from .models import Sport,Contact
 
-admin.site.register(Sport)
+@admin.register(Sport)
+class SportAdmin(admin.ModelAdmin):
+    list_display = ("name", "venue", "price", "datetime", "minimumPlayers", "maximumPlayers")
+    list_filter = ("venue", "price", "datetime", "minimumPlayers", "maximumPlayers")
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "phone","message")
--- api/models.py
+++ api/models.py
@@ -22,3 +22,12 @@ class Sport(models.Model):
 
     def __str__(self):
         return self.name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    phone = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
